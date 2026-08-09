052009b555ecee1243f01b596785aa40b2c9dcda
ardzwatson <ardz.watson@gmail.com>
2026-08-09 13:51:20 +1200

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Pass through all network requests directly without serving cached items
+  event.respondWith(fetch(event.request));
+});
